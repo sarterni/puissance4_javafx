4775c188c2ab0d45139959038c4517d639269d45
sarterni <sarter.nicolas@gmail.com>
2024-06-19 11:14:15 +0200

    updateV1

--- puissance4/src/main/java/com/example/App.java
+++ puissance4/src/main/java/com/example/App.java
@@ -5,18 +5,27 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
         private static Scene scene;
+        int gridSize = 4;
+        int cellSize = 100;
+        int gapSize = 10;
 
         public void start(Stage stage) {
                 Pane racine = new Pane();
-                int gridSize = 4;
-                int cellSize = 100;
-                int gapSize = 10;
-
+                Text text = new Text("Puissance 4");
+                text.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+                text.setFill(Color.BLACK);
+                text.setX(350);
+                text.setY(50);
+                racine.getChildren().add(text);
+                
                 for (int row = 0; row < gridSize; row++) {
                         for (int col = 0; col < gridSize; col++) {
                                 Rectangle cell = new Rectangle(col * (cellSize + gapSize), row * (cellSize + gapSize),
